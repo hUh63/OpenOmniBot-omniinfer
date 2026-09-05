4c5d8c099da8241cdd7043f7b49b4150b27b10a0
hUh63 <3133565923@qq.com>
2026-09-06 00:35:16 +0800

    l10n placeholders + router configure support

--- ui/lib/core/router/go_router_config.dart
+++ ui/lib/core/router/go_router_config.dart
@@ -6,11 +6,24 @@ import '../../features/memory/router_config.dart';
 import '../../features/my/router_config.dart';
 
 class AppRouterConfig {
+  static List<GoRoute> _extraHomeRoutes = const [];
+  static List<GoRoute> _extraWelcomeRoutes = const [];
+
+  static void configure({
+    List<GoRoute> extraHomeRoutes = const [],
+    List<GoRoute> extraWelcomeRoutes = const [],
+  }) {
+    _extraHomeRoutes = extraHomeRoutes;
+    _extraWelcomeRoutes = extraWelcomeRoutes;
+  }
+
   static List<GoRoute> getAllRoutes() {
     return [
       ...homeRoutes,
+      ..._extraHomeRoutes,
       ...taskRoutes,
       ...welcomeRoutes,
+      ..._extraWelcomeRoutes,
       ...memoryRoutes,
       ...myRoutes,
     ];
